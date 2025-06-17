12d73ab887c9b0ca701f278727812479806f0933
W1nchie <to.assasin@gmail.com>
2025-06-17 21:37:07 +0300

    fix: change types of data from bytes-like to string objects

--- RaspberryPi/readSerial.py
+++ RaspberryPi/readSerial.py
@@ -8,14 +8,12 @@ time.sleep(2)
 
 while True:
     _ = ser.readline()[:-2]
-    data1 = ser.readline()[:-2]
-    data2 = ser.readline()[:-2]
-    data3 = ser.readline()[:-2]
-    data4 = ser.readline()[:-2]
+    data1 = str(ser.readline()[:-2])[2:-1]
+    data2 = str(ser.readline()[:-2])[2:-1]
+    data3 = str(ser.readline()[:-2])[2:-1]
+    data4 = str(ser.readline()[:-2])[2:-1]
     _ = ser.readline()[:-2]
     _ = ser.readline()[:-2]
-    data = ser.readline()[:-2]
-    dataArray = list(map(float, data.split()))
-    accXYZ = list(map(float, data4[23:].split(", ")))
-    gyroYaw = float(data3[12:])
-    print(*accXYZ, gyroYaw)
+    accXYZ = list(map(float, data4[24:].replace(',', '').split()))
+    gyroYaw = float(data3[14:])
+    print(f'dX: {accXYZ[0]}, dY: {accXYZ[1]}, dZ: {accXYZ[2]}, Yaw: {gyroYaw}')

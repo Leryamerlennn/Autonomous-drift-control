414cd0f0955f0d877eee7e6d89677ab24f42b268
W1nchie <to.assasin@gmail.com>
2025-06-17 21:00:34 +0300

    feat: edit code for current Arduino IMU output reading

--- RaspberryPi/readSerial.py
+++ RaspberryPi/readSerial.py
@@ -1,6 +1,3 @@
-#This is a program template that will be further updated
-#  when updating the types and amounts of data on the Arduino.
-
 import serial
 import time
 
@@ -10,6 +7,15 @@ ser = serial.Serial(pathToArduino, baudRate)
 time.sleep(2)
 
 while True:
-    data = ser.readline()[:-2] #Example types: yaw, velX, velY
+    _ = ser.readline()[:-2]
+    data1 = ser.readline()[:-2]
+    data2 = ser.readline()[:-2]
+    data3 = ser.readline()[:-2]
+    data4 = ser.readline()[:-2]
+    _ = ser.readline()[:-2]
+    _ = ser.readline()[:-2]
+    data = ser.readline()[:-2]
     dataArray = list(map(float, data.split()))
-    print(dataArray)
+    accXYZ = list(map(float, data4[23:].split(", ")))
+    gyroYaw = float(data3[12:])
+    print(*accXYZ, gyroYaw)
